a982731f62e823bc22a375101f5cffbbdef6e1d7
Karthik Shettigar <86278623+Karthikkk-24@users.noreply.github.com>
2022-12-19 22:05:23 +0530

    Added Vertical_Image_Gallery

---
+++ Vertical_Image_Gallery/main.js
@@ -0,0 +1,29 @@
+function image_1_trigger() {
+    document.getElementById("pictures").innerHTML =
+        '<img width="100" height="100" src="images/1.jpg">';
+}
+
+function image_2_trigger() {
+    document.getElementById("pictures").innerHTML =
+        '<img width="100" height="100" src="images/2.jpg">';
+}
+function image_3_trigger() {
+    document.getElementById("pictures").innerHTML =
+        '<img width="100" height="100" src="images/3.jpg">';
+}
+function image_4_trigger() {
+    document.getElementById("pictures").innerHTML =
+        '<img width="100" height="100" src="images/4.jpg">';
+}
+function image_5_trigger() {
+    document.getElementById("pictures").innerHTML =
+        '<img width="100" height="100" src="images/5.jpg">';
+}
+function image_6_trigger() {
+    document.getElementById("pictures").innerHTML =
+        '<img width="100" height="100" src="images/6.jpg">';
+}
+
+window.onload = function () {
+    image_1_trigger();
+};
